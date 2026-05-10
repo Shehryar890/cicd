4dccfb6be5042c46aa8c800077a6b1b47f46a6a8
Malik Muhammad Shehryar <sherymalik17786@gmail.com>
2026-05-10 21:02:19 +0500

    Update QuestionappApplication.java

--- src/main/java/com/example/questionapp/QuestionappApplication.java
+++ src/main/java/com/example/questionapp/QuestionappApplication.java
@@ -8,6 +8,7 @@ public class QuestionappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionappApplication.class, args);
+		System.out.println("hey two teh"); 
 	}
 
 }
