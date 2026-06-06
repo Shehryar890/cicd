7d1f67bed4467ee2be8564b890cecd8ac1dac272
Malik Muhammad Shehryar <sherymalik17786@gmail.com>
2026-06-06 16:54:44 +0500

    Update CommentService.java

--- src/main/java/com/example/questionapp/services/CommentService.java
+++ src/main/java/com/example/questionapp/services/CommentService.java
@@ -32,7 +32,7 @@ public class CommentService {
     public List<CommentResponse> getAllComments(Optional<Long> userId, Optional<Long> postId) {
         List<Comment> comments;
         if(userId.isPresent() && postId.isPresent()) {
-            System.out.println("hello");
+          
            
             comments = commentRepository.findByUserIdAndPostId(userId.get(), postId.get());  //get diyince içerisindeki değeri alırız.
         }else if(userId.isPresent()){  //sadece userId gelirse
