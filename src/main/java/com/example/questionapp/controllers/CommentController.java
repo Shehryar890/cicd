c49d7449433b74459444b30b089a6642d757d23e
Malik Muhammad Shehryar <sherymalik17786@gmail.com>
2026-06-06 15:10:37 +0500

    Update CommentController.java

--- src/main/java/com/example/questionapp/controllers/CommentController.java
+++ src/main/java/com/example/questionapp/controllers/CommentController.java
@@ -29,7 +29,7 @@ public class CommentController {
 
     @PostMapping
     public Comment createComment(@RequestBody CreateCommentRequest createCommentRequest ){
-        System.out.println("hello");
+      
         return commentService.createComment(createCommentRequest);
     }
 
